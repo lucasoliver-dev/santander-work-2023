ec47393b99a93cc9b40cf24924dfbd05fb830392
felipeoli7eira <me.felipeoliveira@gmail.com>
2023-09-13 16:11:46 -0300

    Initial commit

---
+++ src/main/java/me/dio/santanderwork2023/SantanderWork2023Application.java
@@ -0,0 +1,13 @@
+package me.dio.santanderwork2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderWork2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderWork2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santanderwork2023/SantanderWork2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santanderwork2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderWork2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
